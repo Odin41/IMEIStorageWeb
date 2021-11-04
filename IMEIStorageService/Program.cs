5f43096ae5f4c46e5c016ad893794f4184ab3509
Odin41 <odin3476@gmail.com>
2021-11-04 09:09:59 +0400

    appsetings Kestrel urls

--- IMEIStorageService/Program.cs
+++ IMEIStorageService/Program.cs
@@ -20,7 +20,7 @@ namespace IMEIStorageService
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>().UseUrls("http://*:5000");
+                    webBuilder.UseStartup<Startup>();//.UseUrls("http://*:5000");
                 });
     }
 }
